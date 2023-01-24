c9a788520dad92e31efac07a0ab3f9d3d61ab398
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-01-24 14:31:58 +0530

    Add files via upload

---
+++ function2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void printtype();//prototype
+void agetype();//prototype
+int main()
+{
+
+   printtype();agetype();//function call
+   printtype();agetype();//function call
+   printtype();agetype();//function call
+
+    return 0;
+}
+void printtype()//function definition
+{
+    printf("i am jyoti sulakhe\n");
+    printf(" i am from orange city\n\n");
+}
+void agetype()//function definition
+{
+    int age;
+    printf("enter age : ");
+    scanf("%d",&age);
+    printf("my age is %d\n",age);
+    }
